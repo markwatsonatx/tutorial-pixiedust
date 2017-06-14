465f970ac6107ee42ed5d7dd533d48da82de1ae4
Mark Watson <mark.watson.x@gmail.com>
2017-06-13 20:55:05 -0500

    Updated import script

--- import/import.sh
+++ import/import.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-statusCode="$(curl -s -o /dev/null -w "%{http_code}" --head http://db/dsads)"
+statusCode="$(curl -f -s -o /dev/null -w "%{http_code}" --head http://db/dsads)"
 while [ $statusCode -ne "200" ]; do
     echo "$(date) - Trying to connect to Cloudant"
-    statusCode="$(curl -s -o /dev/null -w "%{http_code}" --head http://db/)"
+    statusCode="$(curl -f -s -o /dev/null -w "%{http_code}" --head http://db/)"
 	sleep 3
 done
-statusCode="$(curl -X PUT http://admin:pass@db/sxswsessions)"
+statusCode="$(curl -f -X PUT http://admin:pass@db/sxswsessions)"
 while [ $statusCode -ne "200" ]; do
     echo "$(date) - Trying to create database"
-    statusCode="$(curl -X PUT http://admin:pass@db/sxswsessions)"
+    statusCode="$(curl -f -X PUT http://admin:pass@db/sxswsessions)"
         sleep 3
 done
 curl -d @/usr/import/sxswsessions.json -H "Content-Type: application/json" -X POST http://admin:pass@db/sxswsessions/_bulk_docs
