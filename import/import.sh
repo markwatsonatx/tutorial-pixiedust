ab1809370f84b79e41cdf74ef974045350dd89d0
Mark Watson <mark.watson.x@gmail.com>
2017-06-13 21:00:05 -0500

    Updated import script

--- import/import.sh
+++ import/import.sh
@@ -4,19 +4,19 @@ statusCode="$(curl -f -s -o /dev/null -w "%{http_code}" --head http://db/dsads)"
 while [ $statusCode -ne "200" ]; do
     echo "$(date) - Retrying to connect to Cloudant"
     statusCode="$(curl -f -s -o /dev/null -w "%{http_code}" --head http://db/)"
-	sleep 3
+    sleep 3
 done
 echo "$(date) - Trying to create database""
 statusCode="$(curl -f -X PUT http://admin:pass@db/sxswsessions)"
 while [ $statusCode -ne "200" ]; do
     echo "$(date) - Retrying to create database"
     statusCode="$(curl -f -X PUT http://admin:pass@db/sxswsessions)"
-        sleep 3
+    sleep 3
 done
 echo "$(date) - Trying to import data"
 statusCode="$(curl -f -d @/usr/import/sxswsessions.json -H "Content-Type: application/json" -X POST http://admin:pass@db/sxswsessions/_bulk_docs)"
 while [ $statusCode -ne "200" ]; do
     echo "$(date) - Retrying to import data"
-    statusCode="$(curl -f -X PUT http://admin:pass@db/sxswsessions)"
-        sleep 3
+    statusCode="$(curl -f -d @/usr/import/sxswsessions.json -H "Content-Type: application/json" -X POST http://admin:pass@db/sxswsessions/_bulk_docs)"
+    sleep 3
 done
